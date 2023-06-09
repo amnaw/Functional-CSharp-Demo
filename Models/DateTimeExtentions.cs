ac2b4808c8ab31a3a6118c57457c50e61db5df97
Amna Alwzrah <aminah.l.1@windowslive.com>
2023-06-09 11:39:08 +0300

    first commit

---
+++ Models/DateTimeExtentions.cs
@@ -0,0 +1,18 @@
+﻿namespace Modeles;
+public static class DateTimeExtentions
+{
+    // the following code is based on Refrencial Trasparancy
+
+    public static IEnumerable<(int year, int month)> GetYearMonths(this DateTime time) =>
+        //Enumerable.Range(1, 12).Select(month => (time.Year, month));
+        time.Year.GetYearMonths();
+
+    public static IEnumerable<(int year, int month)> GetYearMonths(this int year) =>
+        Enumerable.Range(1, 12).Select(month => (year, month));
+
+    public static IEnumerable<(int year, int month)> GetDecadeMonths(this DateTime time) =>
+        //Enumerable.Empty<(int, int)>();
+        Enumerable.Range(time.Year.ToDacadeBeginning(), 10).SelectMany(GetYearMonths);
+
+    private static int ToDacadeBeginning(this int year) => year / 10 * 10 + 1;
+}
