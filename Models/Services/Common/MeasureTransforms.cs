2723e57a946fc708feebfd1b8cb98725e8537f29
Amna Alwzrah <aminah.l.1@windowslive.com>
2023-06-14 12:09:11 +0300

    Discriminated Unions

---
+++ Models/Services/Common/MeasureTransforms.cs
@@ -0,0 +1,67 @@
+﻿using Models.Types;
+
+namespace Models.Services.Common;
+
+// Discriminated Unions
+public static class MeasureTransforms
+{
+    // Patteren matching type test
+    public static (Measure a, Measure b) SplitInHalves(this Measure m) =>
+        //(default!, default!);
+        m switch
+        {
+            DiscreteMeasure d =>
+                (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 }),
+
+            ContinuousMeasure c when c with { Value = c.Value / 2 } is Measure half => // always true
+                (half, half),
+
+            _ =>
+            throw new InvalidOperationException(
+                $"Not defined for object of type {m?.GetType().Name ?? "<null>"}")
+        };
+
+
+    // General Advice: Seperate techincal code from domain-specific code
+
+
+    private static (Measure a, Measure b) SplitInHalves(this DiscreteMeasure d) =>
+        (d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 });
+
+    private static (Measure a, Measure b) SplitInHalves(this ContinuousMeasure c)
+    {
+        Measure half = c with { Value = c.Value / 2 };
+        return (half, half);
+    }
+
+    public static (Measure a, Measure b) SplitInHalves2(this Measure m) =>
+       m switch
+       {
+           DiscreteMeasure d => d.SplitInHalves(),
+           ContinuousMeasure c => c.SplitInHalves(),
+           _ =>
+           throw new InvalidOperationException(
+               $"Not defined for object of type {m?.GetType().Name ?? "<null>"}")
+       };
+
+    public static (Measure a, Measure b) SplitInHalves3(this Measure m) =>
+    m.AsDiscriminatedUnion() switch
+    {
+        DiscreteMeasure d => d.SplitInHalves(),
+        ContinuousMeasure c => c.SplitInHalves(),
+        _ => default!
+    };
+
+
+    public static (Measure a, Measure b) SplitInHalves4(this Measure m) =>
+        m.MapAny(
+            d => ((d with { Value = (d.Value + 1) / 2 }, d with { Value = d.Value / 2 })),
+
+            c => 
+            {
+                Measure half = c with { Value = c.Value / 2 };
+                return (half, half);
+            }
+            
+         );
+}
---
+++ Models/Types/Measure.cs
@@ -0,0 +1,34 @@
+﻿using Models.Services.Common;
+using System.ComponentModel;
+
+namespace Models.Types;
+
+public abstract record Measure(string Unit);
+
+public record DiscreteMeasure(string Unit, uint Value) : Measure(Unit);
+public record ContinuousMeasure(string Unit, decimal Value) : Measure(Unit);
+
+public static class MeasureExtensions
+{
+    // gaurd method resbonsible of checking approved types
+    public static Measure AsDiscriminatedUnion(this Measure m) =>
+        m switch
+        {
+            DiscreteMeasure or ContinuousMeasure => m,
+
+            _ => throw new InvalidOperationException($"Not defined for object of type {m?.GetType().Name ?? "<null>"}")
+        };
+
+
+    // this requres the union of all mappings 
+    // this method provides a closed set of mappings to the base type
+    public static TResult MapAny<TResult>(this Measure m,
+        Func<DiscreteMeasure, TResult> discrete,
+        Func<ContinuousMeasure, TResult> continuous) =>
+        m.AsDiscriminatedUnion() switch
+        {
+            DiscreteMeasure d => discrete(d),
+            ContinuousMeasure c => continuous(c),
+            _ => default!
+        };
+}
